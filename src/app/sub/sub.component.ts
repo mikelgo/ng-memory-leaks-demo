48e69e625882507e985dc37c86f51697064c5b46
michago <m.gonser@icloud.com>
2020-04-26 13:47:33 +0200

    second example

--- src/app/sub/sub.component.ts
+++ src/app/sub/sub.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-sub',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub.component.css'],
 })
 export class SubComponent implements OnInit {
+  subject = new BehaviorSubject(42);
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subject.subscribe((v) => {
+      const foo = v;
+    });
+  }
 }
