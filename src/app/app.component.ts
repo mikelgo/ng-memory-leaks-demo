368f6a4a55ff873a6f36ad18d22708e1c586df84
michago <m.gonser@icloud.com>
2020-04-26 13:43:24 +0200

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
-  template: `<app-sub></app-sub>`,
+  template: `<app-sub *ngIf="hide"></app-sub>`,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'ng-memory-leak-demo';
   public hide = false;
-  ngOnInit() {}
+  ngOnInit() {
+    // setInterval(() => ((this.hide = !this.hide), 50));
+    interval(50)
+      .pipe(take(10))
+      .subscribe((_) => (this.hide = !this.hide));
+  }
 }
