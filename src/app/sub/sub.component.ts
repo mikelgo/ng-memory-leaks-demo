c1e3ed9833a0a10e198e438ed3433d086ea504b6
michago <m.gonser@icloud.com>
2020-04-26 13:45:57 +0200

    first ex

--- src/app/sub/sub.component.ts
+++ src/app/sub/sub.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub.component.css'],
 })
 export class SubComponent implements OnInit {
+  arr = [];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    for (let i = 0; i < 100000; ++i) {
+      this.arr.push(Math.random());
+    }
+  }
 }
